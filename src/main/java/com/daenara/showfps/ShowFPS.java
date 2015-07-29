2600d056a84859bfbcf01f0a29682dd2f8047e28
Kuroi <Kuroi@Nadine-PC>
2015-07-30 01:46:10 +0200

    First upload

---
+++ src/main/java/com/daenara/showfps/ShowFPS.java
@@ -0,0 +1,18 @@
+package com.daenara.showfps;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = ShowFPS.MODID, version = ShowFPS.VERSION)
+public class ShowFPS
+{
+    public static final String MODID = "showfps";
+    public static final String VERSION = "0.0.1";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+}
