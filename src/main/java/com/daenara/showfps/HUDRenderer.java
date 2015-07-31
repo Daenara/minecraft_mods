869a523ee44242f845f0c7cb2fced5d0bb70648a
Daenara <>
2015-07-31 02:44:51 +0200

    Font size adjustable

--- src/main/java/com/daenara/showfps/HUDRenderer.java
+++ src/main/java/com/daenara/showfps/HUDRenderer.java
@@ -1,26 +1,46 @@
 package com.daenara.showfps;
+import static com.daenara.showfps.UsefullThings.*;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.lwjgl.opengl.GL11;
 
 public class HUDRenderer 
-{
-    public static int infoLineLocX = 1;
-    public static int infoLineLocY = 1;
-    
+{ 
     private static Minecraft mc = Minecraft.getMinecraft();
+    private static FontRenderer fc = mc.fontRendererObj;
+    
     @SubscribeEvent
     public void RenderGameOverlayEvent(RenderGameOverlayEvent event)
     {
-        
-    	//render everything onto the screen
     	if(event.type == RenderGameOverlayEvent.ElementType.TEXT)
     	{
-            //Integer fps = mc.debug;
-            String debugInfo = mc.debug;
-            String fps = String.format("%3s", debugInfo.substring(0, debugInfo.indexOf(" "))).replace(' ', '0') + " FPS";
-            mc.fontRendererObj.drawString(fps, infoLineLocX, infoLineLocY, 0xffffff);
+            writeString(renderFPS(),1,1,0xffffff,0.72F);
     	}
+    }
+    
+    private void writeString(String string, Integer pos_x, Integer pos_y, Integer color, float scale)
+    {
+        GL11.glPushMatrix();
+        GL11.glDisable(GL11.GL_LIGHTING);
+        GL11.glDisable(GL11.GL_DEPTH_TEST);
+        GL11.glScalef(scale, scale, scale);
+        mc.ingameGUI.drawString(fc, string, pos_x, pos_y, color);
+        GL11.glPopMatrix();
+    }
+    
+    private void writeString(String string, Integer pos_x, Integer pos_y, Integer color)
+    { 
+        writeString(string, pos_x, pos_y, color, 1);
+    }
+    
+    
+    private String renderFPS()
+    {
+        String debugInfo = mc.debug;
+        String fps = fillString(debugInfo.substring(0, debugInfo.indexOf(" ")), 3, '0') + " FPS";
+        return fps;
     }
 }
