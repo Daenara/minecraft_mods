77fd56f6ddf45f123baf5e41f7bb16670efa780d
Kuroi <Kuroi@192.168.0.50>
2016-12-20 20:23:14 +0100

    Just set the project up again and renamed a few things

---
+++ src/main/java/net/daenara/simplefps/UsefulThings.java
@@ -0,0 +1,24 @@
+package net.daenara.simplefps;
+
+public class UsefulThings 
+{  
+    public static String fillString(String string, Integer lenght, char filling)
+    {
+        return String.format("%"+lenght+"s", string).replace(' ', filling);
+    }
+    
+    public static String fillString(String string, Integer lenght)
+    {
+        return String.format("%"+lenght+"s", string);
+    }
+    
+    public static void printDebugMessage(String modname, String string)
+    {
+        System.out.println("[" + modname + "] " + string);
+    }
+    
+    public static Integer getColor(String color)
+    {
+	return Integer.parseInt(color, 16);
+    }
+}
